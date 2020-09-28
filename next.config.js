49a8e02c86af57995fe671e1c4fcfe8626b22f39
GoudekettingRM <r.goudeketting@gmail.com>
2020-09-28 10:30:21 +0200

    move uri to .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     MONGO_URI:
-      'mongodb+srv://robinimus:WCwnSgoJaCiVx5CA@cluster0.sazir.mongodb.net/<dbname>?retryWrites=true&w=majority',
+      process.env.MONGO_URI,
   },
 };
